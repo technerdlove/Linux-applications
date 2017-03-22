57494120bbb77cae90c315c9d61c6f7821fecb88
technerdlove <technerdlove@users.noreply.github.com>
2017-03-22 07:27:48 -0700

    Update openldap-client-automated.sh

--- openldap-client-automated.sh
+++ openldap-client-automated.sh
@@ -2,15 +2,31 @@
 
 # Here I install nfs on an Ubuntu machine as a client to nfs on a CentOS 7 server
 
-# Get the internal nfs server ip address from a pre-populated file (at the end nfs-server-automated.sh) and place it in /tmp/nfs-server-ip
-curl -o /tmp/nfs-server-ip https://raw.githubusercontent.com/technerdlove/Linux-applications/master/nfs-server-ip.txt
-
+# Get the internal nfs server ip address from a pre-populated file (at the end openldap-server-automated.sh) and place it in /tmp/openldap-server-ip
+curl -o /tmp/openldap-server-ip https://raw.githubusercontent.com/technerdlove/Linux-applications-companion/master/openldap-server-ip.txt
 # Assign the ip address to a variable named "ipaddress"
-ipaddress=$(cat /tmp/nfs-server-ip)
+ipaddress=$(cat /tmp/openldap-server-ip)
+
+# Get ldap selections from pre-populated file
+curl -o https://raw.githubusercontent.com/technerdlove/Linux-applications-companion/master/ldapselections.txt
 
 # Install nfs on client (ubuntu machine)
 
 #apt-get -y install nfs-client
+
+export DEBIAN_FRONTEND=noninteractive
+ap-get update
+apt-get --yes install libnss-ldap libpam-ldap ldap-utils nslcd debconf-utils
+unset DEBIAN_FRONTEND
+
+while read url
+do
+  curl "$url" >> everywebpage_combined.html
+done < list_of_urls.txt
+
+while read line; do echo "$line" | debconf-set-selections; done < ldapselections.txt
+
+
 
 # Mount the nfs files from the nfs server
 showmount -e $ipaddress 
