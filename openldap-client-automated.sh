7a1340be3e45e3ba0f9c4f0f5d534bd37d3b457c
technerdlove <technerdlove@users.noreply.github.com>
2017-03-22 21:54:43 -0700

    Update openldap-client-automated.sh

--- openldap-client-automated.sh
+++ openldap-client-automated.sh
@@ -23,7 +23,7 @@ unset DEBIAN_FRONTEND
 
 
 # Set ldap selections in debconf usine variable ldapselections
-while read line; do echo "$line" | debconf-set-selections; done < $ldapselections
+while read line; do echo "$line" | debconf-set-selections; done < "$ldapselections"
 
 sleep 3
 
